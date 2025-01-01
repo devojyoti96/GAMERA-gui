751527f546dd498c8703ba75b04611357bf706e0
devojyoti <devojyoti96@gmail.com>
2025-01-01 12:19:35 -0500

    v1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="GAMERA-gui",  # Replace with your project name
-    version="1.0.1",
+    version="1.0.2",
     author="Devojyoti Kansabanik",
     author_email="dkansabanik@ucar.edu",
     description="GAMERA MHD simulation GUI viewer",

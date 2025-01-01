ef1384cda25e8ddc2ca81032171f7f67ccb8f7d3
devojyoti <devojyoti96@gmail.com>
2025-01-01 12:17:55 -0500

    update

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="GAMERA-gui",  # Replace with your project name
-    version="1.0.0",
+    version="1.0.1",
     author="Devojyoti Kansabanik",
     author_email="dkansabanik@ucar.edu",
     description="GAMERA MHD simulation GUI viewer",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/devojyoti96/GAMERA-gui",  # Replace with your repo URL
-    py_modules=["gamera_gui"],              # The script name (without .py)
+    py_modules=["gamera-gui"],              # The script name (without .py)
     entry_points={
         "console_scripts": [
-            "gamera-gui = gamera_gui:main",  # Command-line entry point
+            "gamera-gui = gameragui:main",  # Command-line entry point
         ]
     },
     packages=find_packages(),
